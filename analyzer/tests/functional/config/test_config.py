9128646a2ec568022b1ad5bbadff45ab1bb939d9
Kristóf Umann <dkszelethus@gmail.com>
2022-04-19 17:19:54 +0200

    Scrape together two tests that demonstrate poor conflict resolution for --analyzer-config

--- analyzer/tests/functional/config/test_config.py
+++ analyzer/tests/functional/config/test_config.py
@@ -118,6 +118,48 @@ class TestConfig(unittest.TestCase):
         self.assertIn("clangsa analyzed simple.cpp", out)
         self.assertNotIn("clang-tidy analyzed simple.cpp", out)
 
+    def test_config_file_and_cmd_resolution(self):
+        """
+        TODO
+        """
+        with open(self.config_file_json, 'w+',
+                  encoding="utf-8", errors="ignore") as config_f:
+            json.dump({
+                'analyzer': [
+                    "--analyzer-config",
+                      "clangsa:track-conditions=false",
+                    "--analyzer-config",
+                      "clang-tidy:HeaderFilterRegex=.*"
+                ]}, config_f)
+
+        out, returncode = self.__run_analyze(self.config_file_json, ["--verbose", "debug_analyzer"])
+
+        self.assertEqual(returncode, 0)
+        # FIXME: Both of these should be present.
+        self.assertNotIn("track-conditions=false", out)
+        self.assertIn("{\"HeaderFilterRegex\": \".*\"}", out)
+
+    def test_config_file_and_cmd_resolutionSwitched(self):
+        """
+        TODO
+        """
+        with open(self.config_file_json, 'w+',
+                  encoding="utf-8", errors="ignore") as config_f:
+            json.dump({
+                'analyzer': [
+                    "--analyzer-config",
+                      "clang-tidy:HeaderFilterRegex=.*",
+                    "--analyzer-config",
+                      "clangsa:track-conditions=false"
+                ]}, config_f)
+
+        out, returncode = self.__run_analyze(self.config_file_json, ["--verbose", "debug_analyzer"])
+
+        self.assertEqual(returncode, 0)
+        # FIXME: Both of these should be present.
+        self.assertIn("track-conditions=false", out)
+        self.assertNotIn("{\"HeaderFilterRegex\": \".*\"}", out)
+
     def test_only_clangsa_config_backward_compatible_mixed(self):
         """
         Test the 'analyzer' configuration option backward compatibility.
