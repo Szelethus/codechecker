21a1d50679d677b7a4cc2f364e7b54ea345f3c36
Kristóf Umann <dkszelethus@gmail.com>
2025-05-12 16:25:10 +0200

    remove pass

--- analyzer/tests/functional/codechecker_bazel/test_bazel.py
+++ analyzer/tests/functional/codechecker_bazel/test_bazel.py
@@ -173,7 +173,6 @@ class TestBaseTest(unittest.TestCase):
     def test_bazel_test_clang_ctu_fail(self):
         """Test: bazel test :clang_ctu_fail"""
         # FIXME: This test currently failes in the github CI.
-        pass
         # self.check_command("bazel test :clang_ctu_fail", exit_code=3)
         # logfile = os.path.join(
         #     self.BAZEL_TESTLOGS_DIR, "clang_ctu_fail", "test.log")
